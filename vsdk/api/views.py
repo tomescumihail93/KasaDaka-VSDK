eb897b7f15dbc2a675b4e39012a5c63ed6af343b
Tomescu Mihail <tomescumihail93@gmail.com>
2018-04-30 19:18:10 +0200

    Added dynamic weather forecast, professional stuff

--- vsdk/api/views.py
+++ vsdk/api/views.py
@@ -10,7 +10,7 @@ from .serializers import ForecastSerializer, AlertSerializer
 import urllib
 import simplejson as json
 from pprint import pprint
-import datetime
+from datetime import datetime, timedelta
 
 # Create your views here.
 
@@ -18,15 +18,6 @@ import datetime
 class ForecastList(APIView):
     def get(self, request):
         forecasts = Forecast.objects.all()
-
-        open_weather_response = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast?id=2353257&APPID=784a19283186fe17719114c8027ac5a7&units=metric').read().decode('UTF-8')
-        weather_bit_response = urllib.request.urlopen('https://api.weatherbit.io/v2.0/forecast/3hourly?city_id=2353257&key=d4ab77bb12944aba96933f8f46724b49&units=metric').read().decode('UTF-8')
-        
-        OpenWeatherJson = json.loads(open_weather_response)
-        WeatherBitJson = json.loads(weather_bit_response)
-
-        # parseOpenWeather(OpenWeatherJson, WeatherBitJson)
-
         serializer = ForecastSerializer(forecasts, context={'request': request}, many=True)
         return Response(serializer.data)
 
@@ -36,35 +27,46 @@ class ForecastList(APIView):
 # forecasts/{region_id}/{day}
 class ForecastDetail(APIView):
     def get(self, request, region_id, day):
-        # region = Region.objects.get(region_code=region_id)
-        print(day)
-        forecasts = Forecast.objects.all()
-        serializer = ForecastSerializer(forecasts, context={'request': request}, many=True)
-        return Response(serializer.data)
+        day = int(day)
+        region = Region.objects.get(region_code=region_id)
+        forecastDate = datetime.now()+timedelta(days=(day-1))
+        #check if exists in database
+        forecasts = Forecast.objects.filter(region=region, pub_date__year=forecastDate.year, pub_date__month=forecastDate.month, pub_date__day=forecastDate.day)
+        if(len(forecasts) != 0):
+            serializer = ForecastSerializer(forecasts, context={'request': request}, many=True)
+            return Response(serializer.data)
+        else:
+            open_weather_response = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast?id='+ region_id +'&APPID=784a19283186fe17719114c8027ac5a7&units=metric').read().decode('UTF-8')
+            weather_bit_response = urllib.request.urlopen('https://api.weatherbit.io/v2.0/forecast/3hourly?city_id='+ region_id +'&key=d4ab77bb12944aba96933f8f46724b49&units=metric').read().decode('UTF-8')
+            OpenWeatherJson = json.loads(open_weather_response)
+            WeatherBitJson = json.loads(weather_bit_response)
+            parseOpenWeather(OpenWeatherJson, WeatherBitJson, region_id, forecastDate)
+            forecast_resp = Forecast.objects.filter(region=region, pub_date__year=forecastDate.year, pub_date__month=forecastDate.month, pub_date__day=forecastDate.day)
+            serializer = ForecastSerializer(forecast_resp, context={'request': request}, many=True)
+            return Response(serializer.data)
+
 
 # alerts/{region_id}
 class AlertDetail(APIView):
-    def get(self, request, region_id):
+    def get(self, request, region_id,):
         alerts = Alert.objects.all()
         # forecasts = Forecast.objects.all()
         serializer = AlertSerializer(alerts, context={'request': request}, many=True)
         return Response(serializer.data)
 
-def parseOpenWeather(OpenWeatherJson, WeatherBitJson):
-    region = Region.objects.get(region_code="2353257")
+def parseOpenWeather(OpenWeatherJson, WeatherBitJson, region_id, forecastDate):
+    region = Region.objects.get(region_code=region_id)
+    reqForecast = Forecast()
     for i,val in enumerate(OpenWeatherJson['list']):
         if i % 8 == 0:
             openApiForecast = Forecast()
             openApiForecast.description = OpenWeatherJson['list'][i]['weather'][0]['description']
-            print(float(OpenWeatherJson['list'][i]['main']['temp']))
-            print(float(WeatherBitJson['data'][i]['app_temp']))
             openApiForecast.temperture = str(round((float(OpenWeatherJson['list'][i]['main']['temp']) + float(WeatherBitJson['data'][i]['app_temp']))/2, 2))
             openApiForecast.wind = str(round((float(OpenWeatherJson['list'][i]['wind']['speed']) + float(WeatherBitJson['data'][i]['wind_gust_spd']))/2, 2))
             openApiForecast.wind_angle = str(round((float(OpenWeatherJson['list'][i]['wind']['deg']) + float(WeatherBitJson['data'][i]['wind_dir']))/2, 2))
-            openApiForecast.pub_date = datetime.datetime.strptime(OpenWeatherJson['list'][i]['dt_txt'], '%Y-%m-%d %H:%M:%S').date()
+            openApiForecast.pub_date = datetime.strptime(OpenWeatherJson['list'][i]['dt_txt'], '%Y-%m-%d %H:%M:%S').date()
             openApiForecast.region = region
             openApiForecast.save()
-    region.save()
 
 # def parseWeatherBit(OpenWeatherJson, WeatherBitJson):
 #     print("Hello WB!")
