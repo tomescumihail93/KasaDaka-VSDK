a4be7ed3d3902954e40d929fe53b1b858457837b
Michel-Daniel COJOCARU (25559) <michel.cojocaru@stud.acs.upb.ro>
2018-04-30 19:11:52 +0200

    database tweak

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -91,14 +91,18 @@ WSGI_APPLICATION = 'vsdk.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-# DATABASES = {
-#      'default': {
-# 		 'ENGINE': 'django.db.backends.sqlite3',
-# 		 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-# 	     }}
+#DATABASES = {
+#     'default': {
+#		 'ENGINE': 'django.db.backends.sqlite3',
+#		 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#	     }}
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 #Heroku db connection
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
